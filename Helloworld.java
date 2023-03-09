9380d06e7c3b58c1e2521c9ee373816f340465aa
rdarshan927 <64832773+rdarshan927@users.noreply.github.com>
2023-03-10 03:54:06 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Application {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("* println for new line and print for printing in the same line");
+    }
+ 
+}
